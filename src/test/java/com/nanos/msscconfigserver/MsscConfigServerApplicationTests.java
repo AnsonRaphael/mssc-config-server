0ddbdca201e1ab99bf3c1bec27199b0d370fb9f1
AnsonRaphael <anson550220@gmail.com>
2022-01-14 05:45:21 +0530

    initial commit

---
+++ src/test/java/com/nanos/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nanos.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
